068ba1f4a8e67f2636723cba355ffc18b91d78bf
Maura <maureenamago@gmail.com>
2024-12-10 12:45:50 +0000

    Add dotenv package for environment variable management

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const bodyParser = require("body-parser")
+require("dotenv").config()
 
 app.use(cors())
 app.use(bodyParser.json())
